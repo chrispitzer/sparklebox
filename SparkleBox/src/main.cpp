403a048c262b6e20725c6e0b08c7e997bd4d9eb9
Chrispy <chris.pitzer@urbanairship.com>
2018-05-05 11:58:29 -0700

    changes to ini file

--- SparkleBox/src/main.cpp
+++ SparkleBox/src/main.cpp
@@ -19,7 +19,7 @@ void loop()
   // turn the LED on (HIGH is the voltage level)
   digitalWrite(LED_BUILTIN, HIGH);
   // wait for a second
-  delay(100);
+  delay(1000);
   // turn the LED off by making the voltage LOW
   digitalWrite(LED_BUILTIN, LOW);
    // wait for a second
