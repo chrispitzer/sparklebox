40812b9a65a3313b33c379b39f7224dd670758b3
Dave Camarillo <dave.camarillo@gmail.com>
2018-05-13 17:11:16 -0700

    moved typedef to header

--- SparkleBox/src/directors/dispatch.cpp
+++ SparkleBox/src/directors/dispatch.cpp
@@ -26,11 +26,6 @@ unsigned char selectedDirector = 0;
 CRGB leds[MAX_LEDS];
 CRGB tempLeds[MAX_LEDS];
 
-typedef struct {
-  uint32_t num_leds_configured;
-  CRGB leds[MAX_LEDS];
-  CRGB tempLeds[MAX_LEDS];
-} test_led_struct_t;
 
 test_led_struct_t global_led_struct;
 
--- SparkleBox/src/directors/dispatch.h
+++ SparkleBox/src/directors/dispatch.h
@@ -4,9 +4,14 @@
 
   #define DIRECTOR_DISPATCH_H
 
+typedef struct {
+  uint32_t num_leds_configured;
+  CRGB leds[MAX_LEDS];
+  CRGB tempLeds[MAX_LEDS];
+} test_led_struct_t;
 
   extern test_led_struct_t global_led_struct;
-  
+
   void setupDirectors();
   void dispatchDirector();
 
